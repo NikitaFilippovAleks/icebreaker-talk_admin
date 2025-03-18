32a7e3fd0e08c4d7ebf90bb46e02edf5d4fa967c
Nikita Filippov <nikita.filippov.aleks@mail.ru>
2025-03-18 11:16:52 +0300

    Init

---
+++ src/app/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import router from './router';
+import { RouterView } from 'vue-router';
+
+const app = createApp(RouterView);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
---
+++ src/pages/login/ui/login-page.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const title = 'Login page';
+</script>
+
+<template>
+  <div class="about">
+    <h1>{{ title }}</h1>
+    <CheckEnt />
+  </div>
+</template>
